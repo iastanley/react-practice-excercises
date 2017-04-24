17aab241e533704cea98df9f743b320f327a8ec2
iastanley <iasvks.craigslist@gmail.com>
2017-04-24 15:50:02 -0700

    added navigation-bar component to complete excercise

--- src/components/navigation-bar.js
+++ src/components/navigation-bar.js
@@ -1,3 +1,32 @@
 import React from 'react';
 
 // The NavigationBar component goes here.  It should be the default export.
+export default function NavigationBar(props) {
+
+  const links = props.links.map((link, index) => {
+      return  <li key={index}>
+                <a href={link.href}>
+                  {link.text}
+                </a>
+              </li>
+  });
+
+  return (
+    <div className="header-bar">
+      <h1>{props.title}</h1>
+      <nav className="header-bar-nav">
+        <ul>{links}</ul>
+      </nav>
+    </div>
+  );
+}
+
+NavigationBar.defaultProps = {
+  title: '',
+  links: [
+    {
+      text: '',
+      href: ''
+    }
+  ]
+}
